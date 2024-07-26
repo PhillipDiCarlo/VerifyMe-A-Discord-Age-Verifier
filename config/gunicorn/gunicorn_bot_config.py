46f565852c76f88376cc31b27f4beb73e5ab91a8
Phillip DiCarlo <sasha-1221@hotmail.com>
2024-07-26 04:29:11 -0400

    Removed/renamed files

--- config/gunicorn/gunicorn_bot_config.py
+++
@@ -1,7 +0,0 @@
-bind = "0.0.0.0:5000"
-workers = 1
-worker_class = "asyncio"
-timeout = 120
-loglevel = "info"
-errorlog = "-"
-accesslog = "-"
--- config/gunicorn/gunicorn_stripe_config.py
+++ config/gunicorn/gunicorn_stripe_config.py
@@ -1,7 +1,7 @@
 bind = "0.0.0.0:5431"
-workers = 1
+workers = 3
 worker_class = "sync"
 timeout = 120
-loglevel = "info"
+loglevel = "warning"
 errorlog = "-"
 accesslog = "-"
